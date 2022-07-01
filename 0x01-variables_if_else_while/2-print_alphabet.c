8b319e1be62981f728aa4c74f14204f603af8c86
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-01 15:16:00 -0700

    New commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -Entry
+ * Return - 0 on sucess
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
