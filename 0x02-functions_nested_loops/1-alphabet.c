34ef3a24783be2bac21b18e247e79aa3186560e0
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-05 05:23:57 -0700

    New commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 int alphabet;
 int count;
 count = 0;
-while (count < 10);
+while(count < 10);
 {
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+for(alphabet = 'a'; alphabet <= 'z'; alphabet++);
 {
 _putchar(alphabet);
 }
 count++;
-_putchar('\n')
+_putchar('\n');
 }
 }
