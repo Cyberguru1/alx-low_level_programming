ff413bd05025fd759bf56be4a3c73f68705a9664
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-05 04:40:42 -0700

    New commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include <stdlib.h>                                                                    
-#include <stdio.h>                                                                     
-#include <time.h>                                                                      
-/**                                                                                    
- * main - prints putchar                                                               
- * Return - returns zero on Suce
- */                                                                                    
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - prints putchar
+ * return - returns zero on Sucess
+ */
 int main(void)                                                                         
-{                                                                                      
+{
 printf("_putchar\n");
 return(0);
 }
