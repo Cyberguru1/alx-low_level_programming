b390bcab5e047a95b7f20c3ee89768fa460b4cd0
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-05 05:49:09 -0700

    New commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,13 @@
 /**
  * manin - check the code
  * Return: 0 on sucess
- * print_alphabet_x10() - prints alphabet ten times
+ * print_alphabet_x10 - prints alphabet ten times
  */
 void print_alphabet_x10(void)
 {
 char c;
 int i = 0;
-while (i< 10)
+while (i < 10)
 {
 c = 'a';
 while (c <= 'z')
