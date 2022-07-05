d5e67fbe2f545c29041a46699fa73550462d4c51
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-05 05:47:32 -0700

    New commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  _islower - program to check for lower palazo
+ *  @c: contains value
+ *  Retrun: 0 on palazo
+ *  (code by cyberguru)
+ */
+int _islower(int c)
+{
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
