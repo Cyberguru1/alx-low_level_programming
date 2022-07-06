bb06260de24d98cbae7a8662a0cc49089289c0cc
Hamza Saidu <107911619+Cyberguru1@users.noreply.github.com>
2022-07-05 23:35:34 -0400

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * print_last_digit - print last value of a digit
- * Return: 0 on sucess
- * @n: value to check
+ * print_last_digit - return last digit
+ * @n : number to check
+ * Return:0 or 1
  */
 
 int print_last_digit(int n)
 {
-n = n >= 0 ? n : n * -1;
-int last = n % 10;
-_putchar('0' + last);
-return (last);
+	int nv;
+
+	if (n < 0)
+		nv = -1 * (n % 10);
+	else
+		nv = n % 10;
+
+	_putchar(nv + '0');
+		return (nv);
 }
