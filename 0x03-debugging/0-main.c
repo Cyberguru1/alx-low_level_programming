726adb243fc196bee532d431e4474acaba50ea3f
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-06 19:12:06 -0700

    New commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - checks if a number is positive or negative
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+int i;
+i = 0:
+positive_or_negative(i);
+return (0)
+}
