2a72381f90c2b3ba8d76f732fd857b68926102e9
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-06 19:33:44 -0700

    New commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-	int i;
-	printf("Infinte loop incoming :(\n");
-	i = 0;
-	/* while (i < 10) */
-	{
-		/* putchar(i); */
-	}
-	
-	printf("Infinite loop avoided! \\o/\n");
+int i;
 
-	return (0);
+printf("Infinte loop incoming :(\n");
+
+i = 0;
+
+/* while (i < 10) */
+{
+/* putchar(i); */
+}
+	
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
