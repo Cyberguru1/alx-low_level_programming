8979c84c240e7cf3a2022bdb397325a4b0a2efb3
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-06 19:22:05 -0700

    New commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,12 @@
 #include "main.h"
+
+
 /**
- * main - checks if a number is positive or negative
+ * main - tests fuction that prints if integer is positive or negative
  * Return: 0 on sucess
  */
+
+
 int main(void)
 {
 int i;
