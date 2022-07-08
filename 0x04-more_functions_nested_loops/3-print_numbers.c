d620250e3e5810dd7602bd5a434a1496912c4478
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-08 00:43:38 -0700

    New commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers within the range (0-0).
+ * Return: retruns 0 on success
+ */
+
+void print_numbers(void)
+{
+for(int i = 0, i < 10, i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
