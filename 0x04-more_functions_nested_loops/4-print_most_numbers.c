29a9948d3abf6c4607f3e7552f936428e0b97fc4
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-08 01:16:57 -0700

    New commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,18 @@
 
 /**
  * print_most_numbers - prints number without 2 and 4
- * Return; 0 on succes
+ *
  */
 void print_most_numbers(void)
 {
+
 int i;
+
 for (i = 0; i < 10; i++)
 {
 	if (i != 2 && i != 4)
 	{
-		_putchar(i + '0');
+		_putchar((i % 10) + '0');
 	}
 	_putchar('\n');
 }
