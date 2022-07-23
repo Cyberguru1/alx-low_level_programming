fdd782de9f1562f86b441fbe54cb8149b5a75251
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-22 18:48:47 -0700

    New commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
- *  * main - prints its name, followed by a new line
- *   * @argc: number of commandline arguments
- *    * @argv: list of commandline arguments
- *     *
- *      * Return: 0 Always(success)
- *       */
+ ** main - prints its name, followed by a new line
+ ** @argc: number of commandline arguments
+ ** @argv: list of commandline arguments
+ **
+ ** Return: 0 Always(success)
+ */
 int main(int argc, char **argv)
 {
 		printf("%s\n", argv[0]);
