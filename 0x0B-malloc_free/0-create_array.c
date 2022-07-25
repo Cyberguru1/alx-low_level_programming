2cc88a2b1eb3b0811fd499fa7f3cc00d169a9b29
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-25 10:20:15 -0700

    New commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "mani.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Function to initialize a char
+ * @c: charater to be initialized
+ * @size: size of char
+ * Return: returns null if size = 0 or pointer to the array
+ */
+
+char *create_array(unsigned int size, char c);
+
+
+if (size == 0)
+
+	return ('\0');
+
+char arra[size] = {c};
+
+return (*arra);
