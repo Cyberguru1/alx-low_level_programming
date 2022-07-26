a5cd861b653f54171f545385661b393ff6cb0eb4
Cyberguru1 <hamzacypher34@gmail.com>
2022-07-25 23:12:02 -0700

    New commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, words = 0, f = 0;
+	int i = 0, words = 0, f = 0, n;
 	char *buff;
 
 	if (s2 == NULL || s1 == NULL)
@@ -19,21 +19,33 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (i; s1[i] != '\0'; i++)
-	;
-	for (f; s2[f] != '\0'; f++)
-	;
-
-	buff  =  malloc(sizeof(char) * (f + i + 1));
-
 	while (*s1)
 	{
-		buff[words++] = s1[words++];
+		words++;
+		i++;
+		s1++;
 	}
-	buff[words] = " ";
+
 	while (*s2)
 	{
-		buff[words++] = s2[words++];
+		words++;
+		s2++;
+		f++;
 	}
+
+
+	buff  =  malloc(sizeof(char) * (words + 2));
+
+
+	for (n = 0; n < words + 1; n++)
+	{
+		if (n == i)
+			buff[n] = ' ';
+		if (s1[n] != '\0')
+			buff[n] = s1[n];
+		buff[n] = s2[n];
+	}
+	buff[words + 1] = '\0';
 	return (buff);
+
 }
