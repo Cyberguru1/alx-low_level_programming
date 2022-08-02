fed444e4a2a4eac206b83f6bfd969f6512523e32
Cyberguru1 <hamzacypher34@gmail.com>
2022-08-02 16:54:36 -0700

    New commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - prints the sturct dog
+ * @d: pointer to struckt d
+ * Returns: Null or nill on error
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	if (d->age == NULL)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %s\n", d->age);
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
