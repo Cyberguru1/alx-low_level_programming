a3590f3c6410c88b31cc80859f963b67c72ed872
Cyberguru1 <hamzacypher34@gmail.com>
2022-08-01 17:44:48 -0700

    New commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,7 @@
 /**
  * struct dog - A new type describing a dog.
  *
- * @#name: the name of the dog
+ * @name: the name of the dog
  * @age: the age of the dog.
  * @owner: the owner of the dog.
  *
