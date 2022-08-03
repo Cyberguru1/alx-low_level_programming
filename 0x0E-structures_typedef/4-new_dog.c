716b30b630f4ed8b11308a92d133198df10e93ca
Cyberguru1 <hamzacypher34@gmail.com>
2022-08-02 17:40:49 -0700

    New commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * new_dog - function pointer that creates a new dog
+ * @name: name of new dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Return: returns null if function fa char *owner)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog != NULL)
+	{
+		new_dog->name = name;
+		new_dog->age = age;
+		new_dog->owner = owner;
+	}
+	else
+		return (NULL);
+}
