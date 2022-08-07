fee49f420207642577f4ea54dc61828c3ca44dd4
Cyberguru1 <hamzacypher34@gmail.com>
2022-08-06 19:48:38 -0700

    New commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * array_iteratot - function that returns an array of input
+ * array_iterator - function that returns an array of input
  * @array: array of params
  * @size: size of array
  * @action: pointer to func.
@@ -8,11 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+
 
 	if (array == NULL || action == NULL)
 		return;
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
