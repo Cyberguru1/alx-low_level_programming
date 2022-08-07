db448339bc764cde6941a121930aae7b8d65db6c
Cyberguru1 <hamzacypher34@gmail.com>
2022-08-06 20:23:28 -0700

    New commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - function to return an index of a number n an
+ * @size: size of the array.
+ * @cmp: pointer to function
+ * @array: array of values.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, x;
+
+	if (cmp == NULL || array == NULL)
+		return (0);
+	if (size <= 0)
+		return (-1);
+	while (size-- > 0)
+	{
+		x = cmp(*array);
+		if (x)
+			return (i);
+		i++;
+		array++;
+	}
+}
