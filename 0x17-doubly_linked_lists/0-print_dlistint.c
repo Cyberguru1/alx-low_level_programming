fc06b261dbd360e61eaa4951c77aacf1f6c8db28
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-29 22:33:50 +0100

    task 01

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,12 +2,21 @@
 /**
  * print_dlistint - a function that prints a double linked
  * list
- * @h: input header
- * Return:size of dlinkedlist and prints content
+ * @h: head of list
+ * Return: the number of node
  */
 
 size_t print_dlistint(const dlistint_t *h){
 	size_t n = 0;
+
+	if (h == NULL){
+		return (n);
+	}
+
+	while (h->prev != NULL){
+		h = h->prev;
+	}
+
 
 	while (h != NULL){
 		printf("%d\n",h->n);
