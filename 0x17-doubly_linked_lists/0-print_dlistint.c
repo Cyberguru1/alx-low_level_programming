8c8727a2eaa88cd4a06685d6c67c1952f6ea0677
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-29 22:56:16 +0100

    new task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,28 +1,30 @@
 #include "lists.h"
+
 /**
- * print_dlistint - a function that prints a double linked
- * list
- * @h: head of list
- * Return: the number of node
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
 
-size_t print_dlistint(const dlistint_t *h){
-	size_t n = 0;
+	count = 0;
 
-	if (h == NULL){
-		return (n);
-	}
+	if (h == NULL)
+		return (count);
 
-	while (h->prev != NULL){
+	while (h->prev != NULL)
 		h = h->prev;
-	}
 
-
-	while (h != NULL){
-		printf("%d\n",h->n);
-		n++;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
 
-	return (n);
+	return (count);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,28 +1,29 @@
 #include "lists.h"
+
 /**
- * dlistint_len - a function that prints a double linked
- * list
- * @h: input header
- * Return:size of dlinkedlist
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
 
-size_t dlistint_len(const dlistint_t *h){
-	size_t n = 0;
+	count = 0;
 
+	if (h == NULL)
+		return (count);
 
-	if (h == NULL){
-		return (n);
-	}
-
-	while (h->prev != NULL){
+	while (h->prev != NULL)
 		h = h->prev;
-	}
 
-	while (h != NULL){
-		n++;
+	while (h != NULL)
+	{
+		count++;
 		h = h->next;
 	}
 
-	return (n);
+	return (count);
 }
-
