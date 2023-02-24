e7c0451da89c6a6a3ec1b91086fff812d3284a2f
Cyberguru1 <hamzacypher34@gmail.com>
2023-02-24 11:57:41 -0600

    update

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array
+ * @array: input array
+ * @size: size of the array
+ * @value: value searching for
+ * Return: Always Exit_success
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int j;
+
+	if (array == NULL)
+		return (-1);
+
+	for (j = 0; j < (int)size; j++)
+{
+		printf("Value checked array[%u] = [%d]\n", j, array[j]);
+		if (value == array[j])
+			return (1);
+}
+	return (-1);
+}
