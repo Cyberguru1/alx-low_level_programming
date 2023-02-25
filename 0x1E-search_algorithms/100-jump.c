c00ea0140c10c3450c06d6061616f20c6a6d603b
Cyberguru1 <hamzacypher34@gmail.com>
2023-02-25 00:54:31 -0600

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -35,7 +35,7 @@ int jump_search(int *array, size_t size, int value)
 
 	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printf("Value checked array[%d] == [%d]\n", prev, array[prev]);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		if (array[prev] == value)
 			return (prev);
 	}
